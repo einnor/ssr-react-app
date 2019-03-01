85d57a128c13b961de4bc340fe0f2f9f5858d537
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-03-01 11:54:31 +0300

    Added link to speakerwq

--- src/SpeakerCard.js
+++ src/SpeakerCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Link from 'next/link'
 
 export default class SpeakerCard extends Component {
 	render() {
@@ -7,6 +8,9 @@ export default class SpeakerCard extends Component {
 			<div>
 				<img className="card-img-top" src={`/static/speakers/Speaker-${this.props.speaker.id}.jpg`}/>
                 <div className="card-body">
+                	<Link href={{ pathname: '/speaker', query: { speakerId: speaker.id } }} as={`speaker/${speaker.id}`}>
+                		<a className="btn btn-lg btn-block btn-outline-primary">Details</a>
+                	</Link>
                     <h4 className="card-title">{this.props.speaker.userFirstName} {this.props.speaker.userLastName} </h4>
                     <p className="card-text">{this.props.speaker.bioShort}</p>
                 </div>
