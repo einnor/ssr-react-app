9f77ef0bb66720ac8002ec2e0366c13106973972
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-03-07 15:23:56 +0300

    Added page caching for specifi routes to the node server

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const next = require('next');
+const LRUCache = require('lru-cache');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+const ssrCache = new LRUCache({
+    length: function(n, key) {
+        return n.toString().length + key.toString().length;
+    },
+    max: 100 * 1000 * 1000, // 100MB cache soft limit
+    maxAge: 1000 * 10 // 1000 * 60 * 60 // 1 hour
+});
 
 app.prepare()
     .then(() => {
@@ -16,6 +25,9 @@ app.prepare()
         });
 
         server.get('*', (req, res) => {
+            if (['/', '/speakers', '/sessions'].includes(req.url)) {
+                return renderAndCache(req, res, req.url, {});
+            }
             return handle(req, res)
         });
 
@@ -28,3 +40,28 @@ app.prepare()
         console.error(ex.stack);
         process.exit(1)
     });
+
+async function renderAndCache(req, res, pagePath, queryParams) {
+    const key = getCacheKey(req);
+    if (ssrCache.has(key)) {
+        res.setHeader("x-cache", "HIT");
+        res.send(ssrCache.get(key));
+        return;
+    }
+    try {
+        const html = await app.renderToHTML(req, res, pagePath, queryParams);
+        if (res.statusCode !== 200) {
+            res.send(html);
+            return;
+        }
+        ssrCache.set(key, html);
+        res.setHeader("x-cache", "MISS");
+        res.send(html);
+    } catch (err) {
+        app.renderError(err, req, res, pagePath, queryParams);
+    }
+}
+
+function getCacheKey(req) {
+    return `${req.url}`;
+}
